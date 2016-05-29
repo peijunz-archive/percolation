d16a03a5065bd7ca6ac85f217b375fa7299aa8e4
Peijun Zhu <zpj.ustc@gmail.com>
2016-05-29 00:39:13 -0400

    seperate deployment in an folder

---
+++ deploy/compile.sh
@@ -0,0 +1,2 @@
+cd ..
+g++ -std=c++0x -O3 main.cpp -o deploy/percolation
--- deploy/single.sh
+++ deploy/single.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 #PBS -l walltime=10:00:00:00
 date
-/exports/home/huangwei/zpj/percolation
+/exports/home/huangwei/zpj/deploy/percolation
 date
 
